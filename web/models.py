be532de5c4e0f41899bae356745e5fc3a3822556
Alijafariiii <Alijafari8963@gmail.com>
2024-11-30 14:01:20 +0330

    income models and some beautification

--- web/models.py
+++ web/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Expense(models.Model):
+    text=models.CharField(max_length=255)
+    date=models.DateTimeField()
+    fee=models.BigIntegerField()
+    amount=models.IntegerField()
+    user=models.ForeignKey(User,on_delete=models.PROTECT)
+    def __str__(self):
+        return f"{self.fee} - {self.date}"
+class Income(models.Model):
+    text=models.CharField(max_length=255)
+    date=models.DateTimeField()
+    fee=models.BigIntegerField()
+    amount=models.IntegerField()
+    user=models.ForeignKey(User,on_delete=models.PROTECT)
